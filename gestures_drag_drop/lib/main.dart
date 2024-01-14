29139e464ca4a2662317f6ef91dae8de1e1f674d
thong-thor205N <thongthor205N@gmail.com>
2024-01-14 12:47:00 +0700

    first commit

--- gestures_drag_drop/lib/main.dart
+++ gestures_drag_drop/lib/main.dart
@@ -1,125 +1,163 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() {
-  runApp(const MyApp());
+  runApp(myStateless());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class myStateless extends StatelessWidget {
+  const myStateless({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: "gestures_drap_drop",
+      home: myStateless(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class myStateful extends StatefulWidget {
+  const myStateful({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<myStateful> createState() => _myStatefulState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _myStatefulState extends State<myStateful> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Gestures & Drap and Drop'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              _buildGestureDetector(),
+              Divider(
+                color: Colors.black,
+                height: 44.0,
+              ),
+              _buildDraggable(),
+              Divider(
+                height: 40.0,
+              ),
+              _buildDragTarget(),
+              Divider(
+                color: Colors.black,
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+GestureDetector _buildGestureDetector() {
+  return GestureDetector(
+    onTap: () {
+      print('onTap');
+      _displayGestureDetected('onTap');
+    },
+    onDoubleTap: () {
+      print('onDoubleTap');
+      _displayGestureDetected('onDoubleTap');
+    },
+    onLongPress: () {
+      print('onLongPress');
+      _displayGestureDetected('onLongPress');
+    },
+    onPanUpdate: (DragUpdateDetails details) {
+      print('onPanUpdate: $details');
+      _displayGestureDetected('onPanUpdate:\n$details');
+    },
+//onVerticalDragUpdate: ((DragUpdateDetails details) {
+// print('onVerticalDragUpdate: $details');
+// _displayGestureDetected('onVerticalDragUpdate:\n$details');
+//}),
+//onHorizontalDragUpdate: (DragUpdateDetails details) {
+// print('onHorizontalDragUpdate: $details');
+// _displayGestureDetected('onHorizontalDragUpdate:\n$details');
+//},
+//onHorizontalDragEnd: (DragEndDetails details) {
+// print('onHorizontalDragEnd: $details');
+// if (details.primaryVelocity < 0) {
+// print('Dragging Right to Left: ${details.velocity}');
+// } else if (details.primaryVelocity > 0) {
+// print('Dragging Left to Right: ${details.velocity}');
+// }
+//},
+    child: Container(
+      color: Colors.lightGreen.shade100,
+      width: double.infinity,
+      padding: EdgeInsets.all(24.0),
+      child: Column(
+        children: <Widget>[
+          Icon(
+            Icons.access_alarm,
+            size: 98.0,
+          ),
+          Text('$_gestureDetected'),
+        ],
+      ),
+    ),
+  );
+}
+
+void _displayGestureDetected(String gesture) {
+  setState(() {
+    _gestureDetected = gesture;
+  });
+}
+
+Draggable<int> _buildDraggable() {
+  return Draggable(
+    child: Column(
+      children: <Widget>[
+        Icon(
+          Icons.palette,
+          color: Colors.deepOrange,
+          size: 48.0,
+        ),
+        Text(
+          'Drag Me below to change color',
+        ),
+      ],
+    ),
+    childWhenDragging: Icon(
+      Icons.palette,
+      color: Colors.grey,
+      size: 48.0,
+    ),
+    feedback: Icon(
+      Icons.brush,
+      color: Colors.deepOrange,
+      size: 80.0,
+    ),
+    data: Colors.deepOrange.value,
+  );
+}
+
+DragTarget<int> _buildDragTarget() {
+  return DragTarget<int>(
+    onAccept: (colorValue) {
+      _paintedColor = Color(colorValue);
+    },
+    builder: (BuildContext context, List<dynamic> acceptedData,
+            List<dynamic> rejectedData) =>
+        acceptedData.isEmpty
+            ? Text(
+                'Drag To and see color change',
+                style: TextStyle(color: _paintedColor),
+              )
+            : Text(
+                'Painting Color: $acceptedData',
+                style: TextStyle(
+                  color: Color(acceptedData[0]),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+  );
 }
---
+++ platform_channel/android/app/src/main/kotlin/com/example/platform_channel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.platform_channel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
